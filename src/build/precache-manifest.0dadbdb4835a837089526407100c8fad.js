1142cd2c4ff6f17cb21d18fdbb4029aa9101217e
lakshitmadaan20 <51285500+lakshitmadaan20@users.noreply.github.com>
2020-11-07 15:56:16 +0530

    first commit

---
+++ src/build/precache-manifest.0dadbdb4835a837089526407100c8fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b5b429ef93d447f69e4",
+    "url": "/static/css/main.c3ae66bc.chunk.css"
+  },
+  {
+    "revision": "6b5b429ef93d447f69e4",
+    "url": "/static/js/main.6b5b429e.chunk.js"
+  },
+  {
+    "revision": "fbd101e9e85db562748a",
+    "url": "/static/css/1.2d0d102e.chunk.css"
+  },
+  {
+    "revision": "fbd101e9e85db562748a",
+    "url": "/static/js/1.fbd101e9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3dc83c5194070886996ba38b5d9445f",
+    "url": "/index.html"
+  }
+];
